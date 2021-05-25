386e1926f0c18f2d013d434f2a49d2326e6d1d46
Pruthaa <84495786+Pruthaa@users.noreply.github.com>
2021-05-25 18:53:13 +0530

    Add files via upload

---
+++ India.dart
@@ -0,0 +1,293 @@
+import 'package:flutter/material.dart';
+
+class India extends StatefulWidget {
+  const India({Key key}) : super(key: key);
+
+  @override
+  _IndiaState createState() => _IndiaState();
+}
+
+class _IndiaState extends State<India> {
+  Color bgColor = Colors.white70;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.grey[800],
+        title: Text(
+          'India',
+        ),
+        centerTitle: true,
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(8.0),
+        children: <Widget>[
+          SizedBox(width: 103.0, height: 20.0),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.white70,
+              image: DecorationImage(
+                image: AssetImage('assets/place.jpeg'),
+                fit: BoxFit.fill,
+              ),
+            ),
+
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Text('Tourist Attractions\n',
+                  /*'              •  Taj Mahal\n'
+                    '              •  Red Fort\n'
+                    '              •  Qutub Minar\n'
+                    '              •  Humayuns Tomb\n'
+                    '              •  Agra Fort\n'
+                    '              •  Fatehpur Sikri\n'
+                    '              •  Golden Temple\n'
+                    '              •  Mamleshwar Temple\n',*/
+                  style: TextStyle(
+                    fontSize: 25.0,
+                    fontWeight: FontWeight.bold,
+                    color: bgColor,
+                  ),
+                ),
+                Text(
+                    '              -  Taj Mahal\n',
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      color: bgColor,
+                    )
+                ),
+                Text(
+                    '              -  Red Fort\n',
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      color: bgColor,
+                    )
+                ),
+                Text(
+                    '              -  Qutub Minar\n',
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      color: bgColor,
+                    )
+                ),
+                Text(
+                    '              -  Humayuns Tomb\n',
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      color: bgColor,
+                    )
+                ),
+                Text(
+                    '              -  Agra Fort\n',
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      color: bgColor,
+                    )
+                ),
+                Text(
+                    '              -  Fatehpur Sikri\n',
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      color: bgColor,
+                    )
+                ),
+                Text(
+                    '              -  Golden Temple\n',
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      color: bgColor,
+                    )
+                ),
+                Text(
+                    '              -  Mamleshwar temple\n',
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      color: bgColor,
+                    )
+                ),
+              ],
+            ),
+          ),
+          SizedBox(width: 103.0,height: 20.0),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.white70,
+              image: DecorationImage(
+                image: AssetImage('assets/biryani.jpg'),
+                fit: BoxFit.fill,
+              ),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Text('Famous Dishes\n',
+                  /*'              •  Aloo gobi\n'
+                    '              •  Butter chicken\n'
+                    '              •  Chana masala.\n'
+                    '              •  Palak paneer.\n'
+                    '              •  Chicken tikka masala\n'
+                    '              •  Crisp papadum\n'
+                    '              •  Fish curry\n'
+                    '              •  Chole Bhature\n',*/
+                  style: TextStyle(
+                    fontSize: 25.0,
+                    fontWeight: FontWeight.bold,
+                    color: bgColor,
+                  ),
+                ),
+                Text(
+                    '              -  Aloo gobi\n',
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      color: bgColor,
+                    )
+                ),
+                Text(
+                    '              -  Butter chicken\n',
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      color: bgColor,
+                    )
+                ),
+                Text(
+                    '              -  Chana masala.\n',
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      color: bgColor,
+                    )
+                ),
+                Text(
+                    '              - Palak paneer.\n',
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      color: bgColor,
+                    )
+                ),
+                Text(
+                    '              -  Chicken tikka masala\n',
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      color: bgColor,
+                    )
+                ),
+                Text(
+                    '              -  Crisp papadum\n',
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      color: bgColor,
+                    )
+                ),
+                Text(
+                    '              -  Fish curry\n',
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      color: bgColor,
+                    )
+                ),
+                Text(
+                    '              -  Chole Bhature\n',
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      color: bgColor,
+                    )
+                ),
+              ],
+            ),
+          ),
+          SizedBox(width: 103.0,height: 20.0),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.white70,
+              image: DecorationImage(
+                image: AssetImage('assets/diving1.jpg'),
+                fit: BoxFit.fill,
+              ),
+            ),
+
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Text('Things to do\n',
+                  /*'              •  Scuba Diving in Goa\n'
+                    '              •  FlyBoard in Goa\n'
+                    '              •  Snorkelling in Goa\n'
+                    '              •  River Rafting in Rushikesh\n'
+                    '              •  Sking in Gulmarg\n'
+                    '              •  Rock climbing in Manali\n'
+                    '              •  Road Trip to Ladakh\n'
+                    '              •  Lion safari in Gir\n',*/
+                  style: TextStyle(
+                    fontSize: 25.0,
+                    fontWeight: FontWeight.bold,
+                    color: bgColor,
+                  ),
+                ),
+                Text(
+                    '              -  Scuba Diving in Goa\n',
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      color: bgColor,
+                    )
+                ),
+                Text(
+                    '              -  FlyBoard in Goa\n',
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      color: bgColor,
+                    )
+                ),
+                Text(
+                    '              -  Snorkelling in Goa\n',
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      color: bgColor,
+                    )
+                ),
+                Text(
+                    '              -  River Rafting in Rushikesh\n',
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      color: bgColor,
+                    )
+                ),
+                Text(
+                    '              -  Sking in Gulmarg\n',
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      color: bgColor,
+                    )
+                ),
+                Text(
+                    '              -  Rock climbing in Manali\n',
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      color: bgColor,
+                    )
+                ),
+                Text(
+                    '              -  Road Trip to Ladakh\n',
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      color: bgColor,
+                    )
+                ),
+                Text(
+                    '              -  Lion safari in Gir\n',
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      color: bgColor,
+                    )
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
